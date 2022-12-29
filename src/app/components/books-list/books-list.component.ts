e0b4e5965b4984c43921aea524d602f7f6b6da32
Ruslan <testingemail@fakemail.com>
2022-12-29 16:01:02 +0100

    fin de la première version du front

---
+++ src/app/components/books-list/books-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Book } from 'src/app/domain/book';
+import { BooksService } from 'src/app/services/books.service';
+
+@Component({
+  selector: 'app-books-list',
+  templateUrl: './books-list.component.html',
+  styleUrls: ['./books-list.component.scss']
+})
+export class BooksListComponent {
+
+  books: Observable<Book[]>;
+
+  constructor(private service: BooksService) {
+    this.books = service.getAllBooks();
+  }
+}
---
+++ src/app/domain/book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+  id: number;
+  title: string;
+  authors: string;
+  description: string;
+  price: number;
+  imageURL: string;
+}
